132d806372488ec242fa23820e49302edd54c1b7
ryqdev <ykshang4119@gmail.com>
2024-07-09 20:28:43 +0800

    feat: WIP

--- src/anyparse/anyparse.py
+++ src/anyparse/anyparse.py
@@ -1,2 +1,23 @@
-def parse(original_string: str) -> str:
-    return original_string
+
+
+def parse(original_string: str, target_key_list: list[str]) -> dict[str, str]:
+    ans: dict[str, str] = {}
+    cache = build_cache(original_string)
+    for target in target_key_list:
+        if target in cache.keys():
+            ans[target] = cache[target]
+    return ans
+
+
+
+def build_cache() -> dict[str]:
+    pass
+# def build_cache(original_string: str) -> dict[str]:
+#     cache = dict[str, str]
+#     length: int = len(original_string)
+#     current_key: str = ""
+#     for i in range(length):
+#         if i.isalpha():
+#             current_key += i
+#         else:
+#             current_key = ""
