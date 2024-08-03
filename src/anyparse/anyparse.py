2109ea72c0fe8168a009e93c2269db92a511f555
ryqdev <ykshang4119@gmail.com>
2024-08-03 14:26:18 +0800

    update log info

--- src/anyparse/anyparse.py
+++ src/anyparse/anyparse.py
@@ -1,7 +1,6 @@
 from loguru import logger
 
 
-# a:1, b:2
 def parse(original_string: str, target_key_list: list[str], delimiter: str) -> dict[str, str]:
     ans: dict[str, str] = dict()
     string_list: list[str] = original_string.split(delimiter)
@@ -11,7 +10,7 @@ def parse(original_string: str, target_key_list: list[str], delimiter: str) -> d
         try:
             cache[kv_list[0]] = kv_list[1]
         except:
-            logger.error("bad string format")
+            logger.error("Unexpected string format")
 
     for target in target_key_list:
         if target in cache.keys():
